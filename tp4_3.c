ac2c7216f8950a3851610bd758934c6c199547a9
marianogk <marianokpo7@gmail.com>
2023-04-28 20:56:18 -0300

    implementando lista enlazada

---
+++ tp4_3.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct
+{
+    int TareaID; // Numerado en ciclo iterativo
+    char *Descripcion;
+    int Duracion; // entre 10 – 100
+} typedef Tarea;
+
+typedef struct
+{
+    Tarea T;
+    Nodo *siguiente;
+} Nodo;
+
+void ToDo();
+Tarea BuscarTareaPorId(Tarea *tarea, int cant);
+Tarea BuscarTareaPorPalabra(Tarea *tarea, int cant);
+
+int main()
+{
+    ToDo();
+
+    return 0;
+}
+
+void ToDo()
+{
+    int cant, realizada;
+
+    printf("Ingrese cantidad de tareas: ");
+    scanf("%d", &cant);
+
+    for (int i = 0; i < cant; i++)
+    {
+        arreglo[i] = malloc(sizeof(Tarea));
+
+        arreglo[i]->TareaID = i;
+        printf("Tarea %d \n", arreglo[i]->TareaID);
+
+        char cadena[50];
+        puts("Ingrese la descripcion:");
+        scanf("%s", cadena);
+
+        arreglo[i]->Descripcion = malloc(sizeof(char) * (strlen(cadena) + 1));
+
+        strcpy(arreglo[i]->Descripcion, cadena);
+
+        puts("Ingrese la duracion:");
+        scanf("%d", &arreglo[i]->Duracion);
+    }
+
+    puts("\n-LISTA DE TAREAS-\n");
+    int k = 0;
+
+    for (int i = 0; i < cant; i++)
+    {
+
+        TareasPendientes[i] = arreglo[i];
+
+        printf("\nTAREA %d\n", arreglo[i]->TareaID);
+        printf("Descripcion:");
+        puts(arreglo[i]->Descripcion);
+        printf("Duracion: %d", arreglo[i]->Duracion);
+        printf("\nSe realizo la tarea %d?  1.SI 2.NO:  ", arreglo[i]->TareaID);
+        scanf("%d", &realizada);
+
+        if (realizada == 1)
+        {
+            TareasRealizadas[k] = TareasPendientes[i];
+            TareasPendientes[i] = NULL;
+            k++;
+        }
+    }
+
+    puts("\n\nTAREAS REALIZADAS\n");
+
+    for (int i = 0; i < k; i++)
+    {
+        if (TareasRealizadas[i] != NULL)
+        {
+            printf("\nTAREA %d\n", TareasRealizadas[i]->TareaID);
+            printf("Descripcion: %s", TareasRealizadas[i]->Descripcion);
+            printf("\nDuracion: %d", TareasRealizadas[i]->Duracion);
+        }
+    }
+
+    puts("\n\nTAREAS PENDIENTES\n");
+
+    for (int i = 0; i < cant; i++)
+    {
+        if (TareasPendientes[i] != NULL)
+        {
+            printf("\nTAREA %d\n", TareasPendientes[i]->TareaID);
+            printf("Descripcion: %s", TareasPendientes[i]->Descripcion);
+            printf("\nDuracion: %d", TareasPendientes[i]->Duracion);
+        }
+    }
+
+    // Buscar Tarea
+
+    int opcion;
+
+    puts("\n\nBUSCAR TAREA\n");
+    puts("1. Por Id");
+    puts("2. Por Palabra");
+    scanf("%d", &opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        BuscarTareaPorId(*arreglo, cant);
+        break;
+    case 2:
+        BuscarTareaPorPalabra(*arreglo, cant);
+        break;
+
+    default:
+        puts("\nOpcion incorrecta\n");
+        break;
+    }
+
+    // Liberar memoria
+
+    for (int i = 0; i < cant; i++)
+    {
+        free(arreglo[i]);
+        free(TareasPendientes[i]);
+        free(TareasRealizadas[i]);
+    }
+    free(arreglo);
+    free(TareasPendientes);
+    free(TareasRealizadas);
+}
+
+Nodo *CrearNodo(Tarea tarea)
+{
+    Nodo *nuevo = (Nodo *)malloc(sizeof(Nodo));
+    nuevo->T = tarea;
+    nuevo->siguiente = NULL;
+    return nuevo;
+}
+
+Tarea BuscarTareaPorId(Tarea *tarea, int cant)
+{
+    int id;
+
+    puts("\n\nBUSCAR TAREA \n");
+    printf("Ingrese el ID: ");
+    scanf("%d", &id);
+
+    for (int i = 0; i < cant; i++)
+    {
+        if (tarea[i].TareaID == id)
+        {
+            printf("\nTAREA %d\n", tarea[i].TareaID);
+            printf("Descripcion: %s", tarea[i].Descripcion);
+            printf("\nDuracion: %d", tarea[i].Duracion);
+        }
+        else
+        {
+            puts("\nNo se encuentra la tarea");
+        }
+    }
+}
+
+Tarea BuscarTareaPorPalabra(Tarea *tarea, int cant)
+{
+    char palabra[20];
+
+    puts("\n\nBUSCAR TAREA POR PALABRA \n");
+    printf("Ingrese la palabra: ");
+    scanf("%s", palabra);
+
+    for (int i = 0; i < cant; i++)
+    {
+        if (strstr(tarea[i].Descripcion, palabra) != NULL)
+        {
+            printf("\nTAREA %d\n", tarea[i].TareaID);
+            printf("Descripcion: %s", tarea[i].Descripcion);
+            printf("\nDuracion: %d", tarea[i].Duracion);
+        }
+        else
+        {
+            puts("\nNo se encuentra la tarea");
+        }
+    }
+}
